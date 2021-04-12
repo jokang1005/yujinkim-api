257b59d42d97efa37dd96721846e7c0bfec586ea
Joanne Lee <jokang1005@gmail.com>
2021-04-12 12:49:31 -0500

    used destructured env variables in a ternary so development has no whitelist but production does

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 //1. import environmental variables 
 require("dotenv").config()
 //////and then destructure all the enviornmental variables
-const {PORT, NODE_ENV}
+const {PORT, NODE_ENV} = process.env
 //2. import other dependencies
 const express = require("express")
 const app = express()
@@ -16,4 +16,4 @@ const corsOptions = require("./config/cors")
 //SETUP MIDDLEWARE//
 ///////////////////////////
 //1. first middleware should always be cors to prevent unauthorized access to server. look at npmjs.com and look at cors documentation. use config folder for misc type info.
-app.use(cors())
+app.use(NODE_ENV === "production" ? cors(corsOptions) : cors())
